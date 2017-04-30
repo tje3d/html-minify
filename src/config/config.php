fb29ce5e88a093603eb35320444ae5c3d0448ba3
tje3d <tje3d@yahoo.com>
2017-05-01 03:14:48 +0430

    Make it EZ

--- src/config/config.php
+++ src/config/config.php
@@ -1,16 +1,5 @@
-<?php return [
+<?php
 
-    // Turn on/off minification
-    'enabled' => true,
-
-    // Turn on/off the comment stripping in the minified file
-    'comment_stripping' => true,
-
-    // If you are using a javascript framework that conflicts
-    // with Blade's tags, you can change them here
-    'blade' => [
-		'rawTags' => ['{!!', '!!}'],
-        'contentTags' => ['{{', '}}'],
-        'escapedContentTags' => ['{{{', '}}}']
-    ]
+return [
+    'enabled' => true
 ];
